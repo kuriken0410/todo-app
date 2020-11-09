3f037f46393c0fb243182c1bbd05b26780239b6c
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-09 16:55:45 +0900

    [add]ajax-request

--- app/javascript/task.js
+++ app/javascript/task.js
@@ -2,6 +2,23 @@ $(document).ready(function() {
   $("#submit").on('click', function(e) {
     e.preventDefault();
     var text = $('#task_title').val()
-    console.log(text)
+
+    $.ajax({
+      // ①HTTPメソッドを指定。
+      type: 'POST',
+      // ②パスを指定.
+      url: '/tasks',
+      // ③HTTPリクエストのボディを指定。Railsではここで指定した値がparamsに入る。
+      data: {
+        task: {
+          title: text,
+        }
+      },
+      // ④HTTPリクエストで要求するdataのタイプを指定。
+      dataType: 'json'
+    // ⑤リクエストに成功した場合 .done以下の関数が実行される。
+    }).done(function(data){
+      console.log(data)
+    })
   })
 })
